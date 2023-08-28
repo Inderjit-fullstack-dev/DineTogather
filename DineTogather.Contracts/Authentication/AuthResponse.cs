7eb35b1af0ae61781f3fc04db130f7b30368eca9
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-08-28 22:09:13 +0530

    initial commit & Authentication request/response setup in constracts

---
+++ DineTogather.Contracts/Authentication/AuthResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace DineTogather.Contracts.Authentication
+{
+    public record AuthResponse
+    (
+        long UserId,
+        string FirstName,
+        string LastName,
+        string Email,
+        string Token
+    );
+}
